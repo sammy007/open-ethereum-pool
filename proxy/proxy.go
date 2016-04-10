911f64eddeb2b1e9ab8048c3b79a818ee1ca36d6
Sammy Libre <sammy007@users.noreply.github.com>
2016-04-10 09:13:43 +0500

    Allow to use mining endpoint URL without /miner prefix

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -108,6 +108,8 @@ func NewProxy(cfg *Config, backend *storage.RedisClient) *ProxyServer {
 func (s *ProxyServer) Start() {
 	log.Printf("Starting proxy on %v", s.config.Proxy.Listen)
 	r := mux.NewRouter()
+	r.Handle("/{login:0x[0-9a-f]{40}}/{id:[0-9a-zA-Z\\-\\_]{1,8}}", s)
+	r.Handle("/{login:0x[0-9a-f]{40}}", s)
 	r.Handle("/miner/{login:0x[0-9a-f]{40}}/{id:[0-9a-zA-Z\\-\\_]{1,8}}", s)
 	r.Handle("/miner/{login:0x[0-9a-f]{40}}", s)
 	srv := &http.Server{
