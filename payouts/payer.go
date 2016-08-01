556a66c6df7f665ae8f95ef72a41b18ae9a581f6
Sammy Libre <sammy007@users.noreply.github.com>
2016-08-01 17:00:15 +0500

    Reduce tx confirmation check interval

--- payouts/payer.go
+++ payouts/payer.go
@@ -15,6 +15,8 @@ import (
 	"github.com/sammy007/open-ethereum-pool/util"
 )
 
+const txCheckInterval = 5 * time.Second
+
 type PayoutsConfig struct {
 	Enabled      bool   `json:"enabled"`
 	RequirePeers int64  `json:"requirePeers"`
@@ -195,7 +197,7 @@ func (u *PayoutsProcessor) process() {
 		// Wait for TX confirmation before further payouts
 		for {
 			log.Printf("Waiting for tx confirmation: %v", txHash)
-			time.Sleep(10 * time.Second)
+			time.Sleep(txCheckInterval)
 			receipt, err := u.rpc.GetTxReceipt(txHash)
 			if err != nil {
 				log.Printf("Failed to get tx receipt for %v: %v", txHash, err)
