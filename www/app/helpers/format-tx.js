c2122c07a6b0562130ad8553ebac51b1f1dcc715
Daniel Böhler <daniel.boehler@hotmail.com>
2016-09-03 11:17:55 +0200

    Format block and transaction hash

--- www/app/helpers/format-tx.js
+++ www/app/helpers/format-tx.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function formatTx(value) {
-  return value[0].substring(2, 26) + "..." + value[0].substring(42);
+  return value[0].substring(2, 6) + ".." + value[0].substring(62);
 }
 
 export default Ember.Helper.helper(formatTx);
