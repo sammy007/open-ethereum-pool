3011990227b83cf983eddc611dc6f7721762ff79
Hakan Yavuz <github@lodos2005.com>
2016-09-11 22:15:58 +0300

    Nicehash Support
    add JSONPushMessageNiceHash

--- proxy/proto.go
+++ proxy/proto.go
@@ -18,9 +18,16 @@ type JSONPushMessage struct {
 	// FIXME: Temporarily add ID for Claymore compliance
 	Id      int64       `json:"id"`
 	Version string      `json:"jsonrpc"`
+	Method string        `json:"method"`
 	Result  interface{} `json:"result"`
 }
-
+type JSONPushMessageNiceHash struct {
+	// FIXME: NiceHash
+	Id      int64       `json:"id"`
+	Version string      `json:"jsonrpc"`
+	Method string        `json:"method"`
+	Params  []string `json:"params"`
+}
 type JSONRpcResp struct {
 	Id      *json.RawMessage `json:"id"`
 	Version string           `json:"jsonrpc"`
