bd1a98da5456b4bd5fd30148bdbeb289ef24e5a2
Sammy Libre <sammy007@users.noreply.github.com>
2017-01-30 11:51:08 +0500

    Fix latest Geth RPC hex encoding

--- payouts/payer.go
+++ payouts/payer.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 
 	"github.com/sammy007/open-ethereum-pool/rpc"
 	"github.com/sammy007/open-ethereum-pool/storage"
@@ -34,12 +35,12 @@ type PayoutsConfig struct {
 
 func (self PayoutsConfig) GasHex() string {
 	x := common.String2Big(self.Gas)
-	return common.BigToHash(x).Hex()
+	return hexutil.EncodeBig(x)
 }
 
 func (self PayoutsConfig) GasPriceHex() string {
 	x := common.String2Big(self.GasPrice)
-	return common.BigToHash(x).Hex()
+	return hexutil.EncodeBig(x)
 }
 
 type PayoutsProcessor struct {
@@ -171,7 +172,7 @@ func (u *PayoutsProcessor) process() {
 			break
 		}
 
-		value := common.BigToHash(amountInWei).Hex()
+		value := hexutil.EncodeBig(amountInWei)
 		txHash, err := u.rpc.SendTransaction(u.config.Address, login, u.config.GasHex(), u.config.GasPriceHex(), value, u.config.AutoGas)
 		if err != nil {
 			log.Printf("Failed to send payment to %s, %v Shannon: %v. Check outgoing tx for %s in block explorer and docs/PAYOUTS.md",
