b460223ceed91980bf72d7a412c7bb2e3f13ac17
root <root@srv2.ethermine.ru>
2017-02-10 23:23:30 +0300

    Redis storage: unix domain socket support

--- storage/redis.go
+++ storage/redis.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 	"time"
+	"net"
 
 	"github.com/ethereum/go-ethereum/common"
 	"gopkg.in/redis.v3"
@@ -14,6 +15,7 @@ import (
 )
 
 type Config struct {
+	Network  string `json:"network"`
 	Endpoint string `json:"endpoint"`
 	Password string `json:"password"`
 	Database int64  `json:"database"`
@@ -80,12 +82,24 @@ type Worker struct {
 }
 
 func NewRedisClient(cfg *Config, prefix string) *RedisClient {
-	client := redis.NewClient(&redis.Options{
+	var client *redis.Client
+	if cfg.Network == "unix" {
+	    client = redis.NewClient(&redis.Options{
+		Dialer: func() (net.Conn, error) {
+		    return net.DialTimeout("unix", cfg.Endpoint, 1*time.Second)
+		},
+		Password: cfg.Password,
+		DB:       cfg.Database,
+		PoolSize: cfg.PoolSize,
+	    })
+	} else {
+	    client = redis.NewClient(&redis.Options{
 		Addr:     cfg.Endpoint,
 		Password: cfg.Password,
 		DB:       cfg.Database,
 		PoolSize: cfg.PoolSize,
-	})
+	    })
+	}
 	return &RedisClient{client: client, prefix: prefix}
 }
 
