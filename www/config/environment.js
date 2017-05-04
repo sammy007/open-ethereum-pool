8ea43950d7673510f56ee8278e4327d301245c0f
deacix <info@deacix.de>
2017-05-05 00:16:28 +0200

    Fix

--- www/config/environment.js
+++ www/config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'music-pool-claims',
+    modulePrefix: 'open-musicoin-pool',
     environment: environment,
     rootURL: '/',
     locationType: 'hash',
