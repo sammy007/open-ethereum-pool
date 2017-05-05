4380925a77affbc000dbceaea766d78de00dd434
deacix <info@deacix.de>
2017-05-05 19:56:12 +0200

    fix

--- docker/gmc.sh
+++ docker/gmc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gmc --fast --unlock "0xaab7da0cf922949aae210e970a710e1b4d2e5dc6" --rpc --rpcaddr "127.0.0.1" --rpcport 8545 --password $WALLET_PASSWORD_FILE_PATH
+gmc --fast --unlock "0x328b9cfd311c9c9e60b6826fc5311be812355168" --rpc --rpcaddr "127.0.0.1" --rpcport 8545 --password $WALLET_PASSWORD_FILE_PATH
