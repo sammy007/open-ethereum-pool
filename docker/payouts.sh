217abd464f8d32ca935012a7f2070dcc23087710
Sergej Kunz <info@deacix.de>
2017-05-05 10:39:53 +0200

    Create payouts.sh

---
+++ docker/payouts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Wait until PostgreSQL started and listens on port 5432.
+while [ -z "`netstat -tln | grep 8545`" ]; do
+  echo 'Waiting for gmc to start ...'
+  sleep 1
+done
+echo 'gmc started.'
+
+# Start server.
+echo 'Starting payouts...'
+/opt/open-ethereum-pool/build/bin/open-ethereum-pool /opt/open-ethereum-pool/payouts.json
