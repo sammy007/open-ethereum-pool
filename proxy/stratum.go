58f2fbbfae5c0198cd38058fd476120bf892c4e1
Ellaismer <ellaismer@protonmail.ch>
2017-11-10 13:22:47 +0800

    Make proxy submit fn async

--- proxy/stratum.go
+++ proxy/stratum.go
@@ -65,7 +65,7 @@ func (s *ProxyServer) ListenTCP() {
 		accept <- n
 		go func(cs *Session) {
 			err = s.handleTCPClient(cs)
-			if err != nil {
+			if err != nil || cs.lastErr != nil {
 				s.removeSession(cs)
 				conn.Close()
 			}
