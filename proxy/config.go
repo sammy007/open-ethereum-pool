9955bab76aba60dfb27f07a3ed7ce8302d94e968
Sammy Libre <sammy007@users.noreply.github.com>
2018-09-28 14:04:56 +0500

    Add support for TLS to stratum

--- proxy/config.go
+++ proxy/config.go
@@ -48,10 +48,13 @@ type Proxy struct {
 }
 
 type Stratum struct {
-	Enabled bool   `json:"enabled"`
-	Listen  string `json:"listen"`
-	Timeout string `json:"timeout"`
-	MaxConn int    `json:"maxConn"`
+	Enabled  bool   `json:"enabled"`
+	Listen   string `json:"listen"`
+	Timeout  string `json:"timeout"`
+	MaxConn  int    `json:"maxConn"`
+	TLS      bool   `json:"tls"`
+	CertFile string `json:"certFile"`
+	KeyFile  string `json:"keyFile"`
 }
 
 type Upstream struct {
