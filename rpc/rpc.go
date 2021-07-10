e60ed0f049455f178fee4efab59245da5c370b04
Pavel Krestyannikov <33207527+bymagnum@users.noreply.github.com>
2021-07-11 00:59:32 +0300

    hexutil.Encode

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -13,6 +13,7 @@ import (
 	"sync"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 
 	"github.com/sammy007/open-ethereum-pool/util"
 )
@@ -177,7 +178,7 @@ func (r *RPCClient) GetBalance(address string) (*big.Int, error) {
 
 func (r *RPCClient) Sign(from string, s string) (string, error) {
 	hash := sha256.Sum256([]byte(s))
-	rpcResp, err := r.doPost(r.Url, "eth_sign", []string{from, common.ToHex(hash[:])})
+	rpcResp, err := r.doPost(r.Url, "eth_sign", []string{from, hexutil.Encode(hash[:])})
 	var reply string
 	if err != nil {
 		return reply, err
