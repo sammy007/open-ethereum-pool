9d2cd64c609c158eada2bb90f053b396cd658e1e
phatblinkie <phatblinkie@hotmail.com>
2022-04-29 21:24:27 +0000

    website build fix for missing intl-format-cache (thanks etc)

--- www/build.sh
+++ www/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#Fix intl-format-cache package.
 
+    cp fix/intl-format-cache/src/* node_modules/intl-format-cache/src/
+    cp fix/intl-format-cache/lib/* node_modules/intl-format-cache/lib/
+
+#Build.
 ./node_modules/.bin/ember build --environment production
