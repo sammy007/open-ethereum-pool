68e8065c2ea3ef1197af8c01281da9e53e3942a5
root <root@s486059.srvape.com>
2022-07-24 14:15:04 +0300

    update

--- build/env.sh
+++ build/env.sh
@@ -7,26 +7,7 @@ if [ ! -f "build/env.sh" ]; then
     exit 2
 fi
 
-# Create fake Go workspace if it doesn't exist yet.
-workspace="$PWD/build/_workspace"
-root="$PWD"
-ethdir="$workspace/src/github.com/sammy007"
-if [ ! -L "$ethdir/open-ethereum-pool" ]; then
-    mkdir -p "$ethdir"
-    cd "$ethdir"
-    ln -s ../../../../../. open-ethereum-pool
-    cd "$root"
-fi
-
-# Set up the environment to use the workspace.
-# Also add Godeps workspace so we build using canned dependencies.
-GOPATH="$workspace"
 GOBIN="$PWD/build/bin"
-export GOPATH GOBIN
+export GOBIN
 
-# Run the command inside the workspace.
-cd "$ethdir/open-ethereum-pool"
-PWD="$ethdir/open-ethereum-pool"
-
-# Launch the arguments with the configured environment.
 exec "$@"
---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/sammy007/open-ethereum-pool
+
+go 1.16
+
+require (
+	github.com/ethereum/ethash v0.0.0-20170407112842-f5f0a8b19625
+	github.com/ethereum/go-ethereum v1.10.20
+	github.com/gorilla/mux v1.8.0
+	github.com/yvasiyarov/gorelic v0.0.7
+	gopkg.in/redis.v3 v3.6.4
+)
+
+require (
+	github.com/etclabscore/go-etchash v0.0.0-20210701082654-a7a7229ffb51
+	github.com/garyburd/redigo v1.6.3 // indirect
+	github.com/onsi/gomega v1.20.0 // indirect
+	github.com/yvasiyarov/go-metrics v0.0.0-20150112132944-c25f46c4b940 // indirect
+	github.com/yvasiyarov/newrelic_platform_go v0.0.0-20160601141957-9c099fbc30e9 // indirect
+	gopkg.in/bsm/ratelimit.v1 v1.0.0-20160220154919-db14e161995a // indirect
+)
